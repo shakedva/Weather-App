b04abb93d4cbfc5ae4ae834cd387865d0c5172b5
shakedva <shakediii123@gmail.com>
2022-05-10 19:53:52 +0300

    `ErrorPage` documentation

--- src/Error/ErrorPage.js
+++ src/Error/ErrorPage.js
@@ -1,13 +1,10 @@
-
-
+/**
+ * Reaching this page means that the URL the user is trying to reach is not implemented
+ * @returns {JSX.Element}
+ */
 const ErrorPage = () =>
 {
-
-    return (
-        <h1>
-            This page does not exist
-        </h1>
-    )
+    return <h1> This page does not exist </h1>
 }
 
 export default ErrorPage;
